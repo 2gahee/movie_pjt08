d40ef0e7919da5eee1957ec39ae40449ca675818
te04072 <te04072@gmail.com>
2024-11-11 02:26:19 +0900

    filtering

--- movies/views.py
+++ movies/views.py
@@ -1,35 +1,29 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_safe
 from .models import Movie, Genre
-from .serializers import MovieSerializer, GenreSerializer
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 # Create your views here.
 @require_safe
-# @api_view
 def index(request):
     movies = Movie.objects.all()
-    # serializer = MovieSerializer(movies)
-    context = {
-        'moviesdata' : movies
-    }
+    genres = Genre.objects.all()
+    context = {'movies': movies,
+               'genres': genres}
     return render(request, 'movies/index.html', context)
 
 @api_view(['GET'])
 def filter_genre(request):
-    if request.method == "GET":
+    genre_name = request.GET.get('genre')
+    if genre_name:
+        movies = Movie.objects.filter(genres__name=genre_name)
+    else:
         movies = Movie.objects.all()
-        genres = Genre.objects.all()
-        movies_serializer = MovieSerializer(movies, many=True)
-        genres_serializer = GenreSerializer(genres, many=True)
-        context = {
-            'moviesdata' : movies_serializer.data,
-            'genresdata' : genres_serializer.data
-        }
-        return Response(context)
-
+    
+    movies_data = [{'title': movie.title} for movie in movies]
+    return JsonResponse({'movies': movies_data})
 
 @require_safe
 def recommended(request):
